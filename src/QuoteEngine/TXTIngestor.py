4d24e607bcf0267596d160a0bbe9a17ab1a6a114
Sal Tamay <sal.tamay@outlook.com>
2021-08-21 13:45:29 -0400

    Add Pdf ingestor class using subprocesses

--- src/QuoteEngine/TXTIngestor.py
+++ src/QuoteEngine/TXTIngestor.py
@@ -15,9 +15,9 @@ class TXTIngestor(IngestorInterface):
         quotes = []
 
         with open(path, 'r') as reader:
-            for ln in reader:
-                if ln != "":
-                    parse = ln.split('-')
+            for line in reader:
+                if line != "":
+                    parse = [word.strip() for word in line.split('-')]
                     new_quote = QuoteModel(parse[0], parse[1])
                     quotes.append(new_quote)
 
