85faa8a40a3c2997f07e47f7f298cbe951f6d4bf
Sal Tamay <sal.tamay@outlook.com>
2021-08-21 13:59:16 -0400

    Stript empty spaces between quote body and author

--- src/QuoteEngine/DOCXIngestor.py
+++ src/QuoteEngine/DOCXIngestor.py
@@ -18,7 +18,7 @@ class DOCXIngestor(IngestorInterface):
 
         for para in doc.paragraphs:
             if para.text != "":
-                parse = para.text.split('-')
+                parse = [word.strip() for word in para.text.split('-')]
                 new_quote = QuoteModel(parse[0], parse[1])
                 quotes.append(new_quote)
 
